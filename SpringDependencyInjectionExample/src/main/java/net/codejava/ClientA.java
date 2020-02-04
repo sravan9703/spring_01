ab735943ab2874d10661ce5e5131cb038de6b6e3
aladblr <endeavorbg@outlook.com>
2020-02-04 05:46:08 +0530

    this is xml based way of doing things

---
+++ SpringDependencyInjectionExample/src/main/java/net/codejava/ClientA.java
@@ -0,0 +1,24 @@
+package net.codejava;
+
+public class ClientA implements Client {
+	
+	Service service;
+	
+	public ClientA() {		
+	}
+	
+	public ClientA(Service service) {
+		this.service = service;
+	}
+
+	@Override
+	public void doSomething() {
+		
+		String info = service.getInfo();
+		System.out.println(info);
+	}
+	
+	public void setService(Service service) {
+		this.service = service;
+	}
+}
